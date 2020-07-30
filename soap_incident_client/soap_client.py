e36a9d8b8c579f5838bf63c68ad8b59c0ed8123e
zom <tommaso.fontana.96@gmail.com>
2020-07-30 15:11:57 +0200

    added logging

--- soap_incident_client/soap_client.py
+++ soap_incident_client/soap_client.py
@@ -7,9 +7,13 @@ from .utils import soap_call, logger
 def soap_client(args):
     incident_id = soap_call(args, "search.xml", "search_regex.txt")
     if incident_id is None:
+        logger.info("No id found, executing an insert.")
         args["inquiryID"] = soap_call(args, "insert.xml", "insert_regex.txt")
+        logger.info("Done insert on id %s"%args["inquiryID"])
         acknowledge(args)
     else:
+        logger.info("Found the id %s, going to update it state."%incident_id)
         args["inquiryID"] = incident_id
-        soap_call(args, "update.xml", "update_regex.txt")
-
+        ident_id = soap_call(args, "update.xml", "update_regex.txt")
+        logger.info("Done update on id %s"%ident_id)
+    logger.info("Done!")
