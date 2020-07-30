2cdb22cc39fc695145a2baee51e42c3725497ecd
zom <tommaso.fontana.96@gmail.com>
2020-07-30 15:14:44 +0200

    added password

--- soap_incident_client/client.py
+++ soap_incident_client/client.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import json
 import argparse
@@ -33,4 +34,13 @@ def client():
     logger.debug("Argument received by the client:\n%s"%args)
 
     settings.update(args)
+
+    with open(settings["pw_file"]) as f:
+        match = re.search(settings["pw_regex"], f.read())
+        if match:
+            settings["password"] = match.group(1)
+        else:
+            logger.error("CANNOT FIND PASSWORD at %s with regex %s"%(settings["pw_file"], settings["pw_regex"]))
+            sys.exit(-1)
+
     soap_client(settings)
