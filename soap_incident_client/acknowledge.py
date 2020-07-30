d0db2fd9f7236bf81542dd1da15ae72cb3abf02b
zom <tommaso.fontana.96@gmail.com>
2020-07-30 16:43:08 +0200

    added arguments

--- soap_incident_client/acknowledge.py
+++ soap_incident_client/acknowledge.py
@@ -1,12 +1,12 @@
 import sys
 import time
-import logging
 import requests
 from urllib.parse import quote
+from .utils import logger
 
 def acknowledge(args):
     url = "{neteye_url}/v1/actions/acknowledge-problem".format(**args)
-    logging.info("[AK] acknowledgin host %s on url %s"%(args["host"], url))
+    logger.info("[AK] acknowledgin host %s on url %s"%(args["host"], url))
     headers = {
         'Accept': 'application/json',
     }
@@ -25,7 +25,7 @@ def acknowledge(args):
     )
 
     if r.status_code == 200:
-        logging.info("[AK] OK : %s"%r.json())
+        logger.info("[AK] OK : %s"%r.json())
         return r.json()
     elif r.status_code in [500, 503, 404]:
-        logging.error("[AK] got error: %s"%r.json())
+        logger.error("[AK] got error: %s"%r.json())
