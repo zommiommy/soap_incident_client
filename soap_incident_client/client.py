31bd36b33128c7af159cd0d46dda13049f459e63
zom <tommaso.fontana.96@gmail.com>
2020-07-30 10:00:58 +0200

    changed print

--- soap_incident_client/client.py
+++ soap_incident_client/client.py
@@ -8,20 +8,24 @@ def client():
     parser.add_argument("-d", "--debug", help="Debug mode, print the xml generated and received", action="store_true", default=False)
 
     login_settings = parser.add_argument_group('login settings')
-    login_settings.add_argument("-i", "--identId", help="", type=str, required=True)
-    login_settings.add_argument("-pw", "--password", help="", type=str, required=True)
-    login_settings.add_argument("-p", "--prozess", help="", type=str, required=True)
+    login_settings.add_argument("identId", help="", type=str, nargs ='+', action = 'store')
+    login_settings.add_argument("password", help="", type=str, nargs ='+', action = 'store')
+    login_settings.add_argument("prozess", help="", type=str, nargs ='+', action = 'store')
     search_settings = parser.add_argument_group('search settings')
-    search_settings.add_argument("-l", "--label_monitoring", help="", type=str, required=True)
-    search_settings.add_argument("-sd", "--it_short_desc", help="", type=str, required=True)
+    search_settings.add_argument("label_monitoring", help="", nargs ='+', action = 'store', type=str)
+    search_settings.add_argument("it_short_desc", help="", nargs ='+', action = 'store', type=str)
     insert_settings = parser.add_argument_group('insert settings')
-    insert_settings.add_argument("-se", "--se_severity", help="", type=str, required=True)
-    insert_settings.add_argument("-it", "--inquiry_txt", help="", type=str, required=True)
+    insert_settings.add_argument("se_severity", help="", nargs ='+', action = 'store', type=str)
+    insert_settings.add_argument("inquiry_txt", help="", nargs ='+', action = 'store', type=str)
 
     args = vars(parser.parse_args())
 
     with open(args["settings"], "r") as f:
         settings = json.load(f)
+
+    for k, v in args.items():
+        if type(v) == list:
+            args[k] = " ".join(v)
 
     args["host"] = args["label_monitoring"]
     args["service"] = args["it_short_desc"]
